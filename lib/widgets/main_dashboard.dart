204d9f6032bb6996637a1a8f00fbf3bf76fc84cf
maximealbert <maxime.albert@epitech.digital>
2024-10-22 16:41:08 +0200

    add func : add a new mood with a modal view

--- lib/widgets/main_dashboard.dart
+++ lib/widgets/main_dashboard.dart
@@ -1,6 +1,6 @@
 // lib/widgets/main_dashboard.dart
 
-// ignore_for_file: prefer_const_constructors, avoid_print, prefer_interpolation_to_compose_strings
+// ignore_for_file: prefer_const_constructors, avoid_print, prefer_interpolation_to_compose_strings, unused_field, unused_local_variable
 
 import 'dart:convert';
 import 'package:flutter/material.dart';
@@ -31,6 +31,7 @@ class _MainDashboardState extends State<MainDashboard> {
   String userName = '';
   String userFirstName = '';
   String teamName = '';
+  int userStrapiId = 0; 
   Image userImage = Image(image: NetworkImage('https://petiteshistoiresdessciences.com/wp-content/uploads/2019/12/image-1.png'));
   String todaysDateFormatted = DateFormat('yyyy-MM-dd').format(DateTime.now());
 
@@ -77,6 +78,7 @@ class _MainDashboardState extends State<MainDashboard> {
           final userPPMedium = userPPLvlTwo['medium'];
           final urlImage = 'http://localhost:1337' + userPPMedium['url'];
           userImage = Image(image: NetworkImage(urlImage,));
+          userStrapiId = userDatas['id'];
 
           final moodsList = userDatas['moods'].toList();
           // print(moodsList);
@@ -223,7 +225,10 @@ class _MainDashboardState extends State<MainDashboard> {
             SizedBox(height: 30,),
             
             // BLOC 1 : mood du jour (selon si la variable todaysMood est cide )
-            todaysMood != null ? TodaysMoodFilled(moodForToday: todaysMood,) : MoodNotFilled(),
+            //todaysMood != null ? TodaysMoodFilled(moodForToday: todaysMood,) : MoodNotFilled(),
+
+            // TODO: dev en cours - a changer
+            MoodNotFilled(documentId: widget.documentIdForSelectedUser, userStrapiId : userStrapiId),
             
 
             
@@ -342,6 +347,7 @@ class TodaysMoodFilled extends StatefulWidget {
 
 class _TodaysMoodFilledState extends State<TodaysMoodFilled> {
 
+
   // controllers
   String percentageToday = '';
   String tagToday = '';
@@ -427,13 +433,150 @@ class _TodaysMoodFilledState extends State<TodaysMoodFilled> {
 }
 
 class MoodNotFilled extends StatefulWidget {
-  const MoodNotFilled({super.key});
+
+  final documentId;
+  final userStrapiId;
+
+  const MoodNotFilled({super.key, required this.documentId, required this.userStrapiId});
 
   @override
   State<MoodNotFilled> createState() => _MoodNotFilledState();
 }
 
 class _MoodNotFilledState extends State<MoodNotFilled> {
+
+  // Controllers
+  double _sliderValue = 0.0;
+  String? selectedTag;
+
+  // TODO: définir les tags (dynamique depuis Strapi ?)
+  final tagsList = <String>['Burn-out', 'Épuisé', 'Fatigué', 'Stressé', 'Débordé', 'Anxieux', 'Frustré', 'Irrité', 'Sous pression', 'Nerveux', 'Déconcentré', 'Distrait', 'Appréhensif', 'Indifférent', 'Fatigué mais motivé', 'Calme', 'Posé', 'Optimiste', 'Motivé', 'Serein', 'Confiant', 'Inspiré', 'Créatif', 'Energique', 'Enthousiaste', 'Heureux', 'Joyeux', 'Exalté', 'Rayonnant', 'Enjoué'];
+
+  // date config
+  DateTime? _selectedDate;
+  final DateFormat _dateFormatter = DateFormat('yyyy-MM-dd');
+  String todaysDateFormatted = DateFormat('yyyy-MM-dd').format(DateTime.now());
+  // User data is transfer with the user's documentId
+
+  String _feelingEmoji = '😁';
+
+  String _getEmoji (int sliderValueVoid){
+    if (sliderValueVoid >= 0 && sliderValueVoid <= 20){
+      return '😩';
+    }else if (sliderValueVoid > 20 && sliderValueVoid <= 40){
+      return '😕';
+    }else if (sliderValueVoid > 40 && sliderValueVoid <= 60){
+      return '😐';
+    }else if (sliderValueVoid > 60 && sliderValueVoid <= 80){
+      return '🙂';
+    }else{
+      return '😃';
+    }
+  }
+
+
+
+  Future<void> addMood() async {
+
+    // Structure the data 
+    final newMoodData = jsonEncode({
+      "data" : {
+         "percentage" : _sliderValue,
+         "mood_datetime" : todaysDateFormatted,
+         "tag" : selectedTag,
+         "user" : [
+          {
+            "id" : widget.userStrapiId
+          }
+         ] 
+      }
+    });
+
+    // Define the URL
+    final url = Uri.parse('http://localhost:1337/api/moods');
+
+    // Define the headers
+    final headers = {
+      "Content-type" : "application/json",
+      "Authorization" : "Bearer 01622abb9cee851ac33e52935f57327301e841ecbeb33d436ec8ca003d55c930416b0c19279b96027bb09d63a65cbd1e3b9149ff5b08151c8383b0831fe1cd22cbfc8f51105e37d0d6b3a4d87cfc9ac33bd66c4e7272eb6b88dd458de4bf753d11d90c37b65e3926c6ebfd86ed486f3c11ff3cc9bf91435b5f03538a8ed478ba "
+    };
+
+    final response = await http.post(url, headers: headers, body: newMoodData);
+
+    if (response.statusCode == 200 || response.statusCode == 201){
+        // Response OK
+        // perform actions here 
+        Navigator.of(context).pop();
+    }else{
+      print("Failed to add mood : ${response.body}");
+    }
+
+
+
+
+
+  }
+
+
+  void _showModalAddMood(){
+    showDialog(context: context, builder: (BuildContext context){
+        return AlertDialog(
+          
+          title: Text('Add your mood'),
+          content: StatefulBuilder(
+          
+              builder: (context, setState) {
+                  return Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+            children:  [
+
+              Text('Comment vous sentez-vous aujourd\'hui ?'),
+              Row(
+                children: [
+                  Text(_sliderValue.toDouble().toStringAsFixed(1)),
+                  Slider(value: _sliderValue, min: 0, max: 100, divisions: 10, label: _getEmoji(_sliderValue.toInt()) ,onChanged: (double value)  {
+                    setState(() {
+                      _sliderValue = value;
+                    });
+                  })
+
+                  ],
+              ),
+              SizedBox(height: 30,),
+              Text('Comment définiriez-vous votre mood ?'),
+              DropdownButton<String>(
+                value: selectedTag,
+                hint: Text('Option'),
+                items: tagsList.map((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                onChanged: (String? newValue) {
+                  setState(() {
+                    selectedTag = newValue; // Mettre à jour la sélection
+                  });
+                },
+              ),
+
+            ],
+          );
+              },
+
+            ),
+          actions: [
+            TextButton.icon(onPressed: (){Navigator.of(context).pop();}, icon: Icon(Icons.close), label: Text('Annuler')),
+            TextButton.icon(onPressed: (){
+              // call method here to add the mood to Strapi
+              addMood();
+            }, icon: Icon(Icons.add), label: Text('Enregistrer')),
+          ],
+        );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     
@@ -454,24 +597,8 @@ class _MoodNotFilledState extends State<MoodNotFilled> {
             child: FilledButton.icon(
               
               onPressed: (){
-                showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return AlertDialog(
-                          title: Text('Dialog Title'),
-                          content: Text('This is a modal dialog.'),
-                          actions: <Widget>[
-                            TextButton(
-                              child: Text('Close'),
-                              onPressed: () {
-                                Navigator.of(context).pop(); // Ferme la boîte de dialogue
-                              },
-                            ),
-                          ],
-                        );
-                      },
-                    );
-              }, 
+                  _showModalAddMood();
+                }, 
               icon: Icon(Icons.add), label: Text('Ajouter votre mood du jour')
             ),
           )
